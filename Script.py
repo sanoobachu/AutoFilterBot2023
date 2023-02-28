14ef7cdf527c82510f0d94ba050c42654d1c1ed4
Sanoob <90376429+sanoobachu@users.noreply.github.com>
2023-03-01 01:52:25 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,14 +3,14 @@ class script(object):
 ᴍʏ ɴᴀᴍᴇ ɪs <a href=https://t.me/{}>{}</a>, ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴍᴏᴠɪᴇs ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴇɴᴊᴏʏ 😍"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 ʜᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ ғᴏʀ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TechMagazineYT>TechMagazine</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: KOYEB
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-    SOURCE_TXT = """<b>NOTE:</b>
+    ABOUT_TXT = """✯ ᴍʏ ɴᴀᴍᴇ: {}
+✯ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/TechMagazineYT>TechMagazine</a>
+✯ ʟɪʙʀᴀʀʏ: ᴘʏʀᴏɢʀᴀᴍ
+✯ ʟᴀɴɢᴜᴀɢᴇ: ᴍᴀʟᴀʏᴀʟᴀᴍ & ᴇɴɢʟɪsʜ
+✯ ᴅᴀᴛᴀ ʙᴀsᴇ: ᴍᴏɴɢᴏ ᴅʙ
+✯ ʙᴏᴛ sᴇʀᴠᴇʀ: ᴋᴏʏᴇʙ
+✯ ʙᴜɪʟᴅ sᴛᴀᴛᴜs: v1.0.1 [ ʙᴇᴛᴀ ]"""
+    SOURCE_TXT = """<b>ɴᴏᴛᴇ:</b>
 - TechMagazine-AutoFilterBot is a open source project. 
 - Source - https://github.com/itsyogieu/TechMagazine-AutoFilterBot
 
